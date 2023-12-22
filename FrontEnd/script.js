19fc0234f0437d75360f7f47aeedaa87fd9c82cb
DEBUIRE Alexis <alexisdebuire@gmail.com>
2023-12-22 13:45:48 +0100

    Filters now setup and working

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,28 +1,35 @@
 //Base script for the main page of Sophie Bluel's Portfolio
-
+//import {  } from "./.js";
 //Function that generate the HTML code for the works
-async function generateWorks() {
-  // Get the works from the database
-  const worksList = await fetch("http://localhost:5678/api/works");
-  const worksListJson = await worksList.json();
-  //Memorise the categories of all works, manually adding filter :"Tous"
-  const categorysOfWorks = new Set();
-  categorysOfWorks.add("Tous");
-
+async function generateWorks(worksListJson) {
   // Loop to create HTML code for all works
   for (let i = 0; i < worksListJson.length; i++) {
     singleWork(worksListJson[i]);
-    // This Set recover every category
-    categorysOfWorks.add(worksListJson[i].category.name);
   }
-  createFilters(categorysOfWorks);
+}
+
+// Recover all categories to use them as filter
+function generateFilters(worksListJson) {
+  //Memorise the categories of all works, manually adding filter :"Tous"
+  const categorysOfWorks = new Set();
+  categorysOfWorks.add("Tous");
+  const listOfCategoryObject = [{ id: 0, name: "Tous" }];
+  // This Set recover every category then we add the category in an Array of object
+  for (let i = 0; i < worksListJson.length; i++) {
+    if (!categorysOfWorks.has(worksListJson[i].category.name)) {
+      categorysOfWorks.add(worksListJson[i].category.name);
+      listOfCategoryObject.push(worksListJson[i].category);
+    }
+  }
+
+  createFilters(listOfCategoryObject);
 }
 
 //Function that create Tags for 1 single work
 async function singleWork(workSingle) {
   const gallery = document.querySelector(".gallery");
   const workBody = document.createElement("figure");
-
+  workBody.dataset.categoryId = workSingle.category.id;
   workBody.innerHTML = `<img src="${workSingle.imageUrl}" alt="${workSingle.title}"/>
   <figcaption>${workSingle.title}</figcaption>`;
   gallery.appendChild(workBody);
@@ -34,16 +41,53 @@ function createFilters(categorysOfWorks) {
   filtersDiv.classList = "filters";
   const portfolioBody = document.querySelector("#portfolio");
   const galleryWorks = document.querySelector(".gallery");
-  console.log(categorysOfWorks);
+
   for (let categorySingle of categorysOfWorks) {
-    const filterSingle = document.createElement("h3");
-    filterSingle.innerText = categorySingle;
+    const filterSingle = document.createElement("button");
+    filterSingle.classList = "filterSingle";
+    filterSingle.innerText = categorySingle.name;
+    filterSingle.dataset.id = categorySingle.id;
     filtersDiv.appendChild(filterSingle);
-    console.log(categorySingle);
   }
-  //portfolioBody.appendChild(filtersDiv);
+
   portfolioBody.insertBefore(filtersDiv, galleryWorks);
+  // Adding class selected to the defautlt filter
+  const listFilters = document.querySelectorAll(".filterSingle");
+  listFilters[0].classList.add("selected");
+}
+//Create Listener for the filters, then display accodringly
+function addListenerFilters(worksListJson) {
+  const filters = document.querySelectorAll(".filterSingle");
+  for (let i = 0; i < filters.length; i++) {
+    filters[i].addEventListener("click", async function (event) {
+      const idCategory = event.target.dataset.id;
+      const allFilter = document.querySelectorAll(".filterSingle");
+      for (let i = 0; i < allFilter.length; i++) {
+        allFilter[i].classList.remove("selected");
+      }
+      event.target.classList.add("selected");
+      /*const awnser = await fetch("http://localhost:5678/api/works");
+      const workByCategory = await awnser.json();*/
+      const workByCategory = Array.from(worksListJson);
+      const gallery = document.querySelector(".gallery");
+      gallery.innerHTML = "";
+      if (parseInt(idCategory) !== 0) {
+        for (let i = workByCategory.length - 1; i >= 0; i--) {
+          if (workByCategory[i].category.id !== parseInt(idCategory)) {
+            workByCategory.splice(i, 1);
+          }
+        }
+      }
+
+      generateWorks(workByCategory);
+    });
+  }
 }
 
 // Main Script
-generateWorks();
+// Get the works from the database
+const worksList = await fetch("http://localhost:5678/api/works");
+const worksListJson = await worksList.json();
+generateWorks(worksListJson);
+generateFilters(worksListJson);
+addListenerFilters(worksListJson);
